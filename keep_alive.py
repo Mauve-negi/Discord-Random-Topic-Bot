fe82c1fe501ab9283eae4c190f96725b032847a7
Mauve-negi <116134417+Mauve-negi@users.noreply.github.com>
2025-04-16 01:47:12 +0000

    ヘルスチェック対応化

--- keep_alive.py
+++ keep_alive.py
@@ -3,15 +3,18 @@ from threading import Thread
 
 app = Flask(__name__)
 
-
+# メイン表示
 @app.route('/')
 def home():
     return "I'm alive!"
 
+# ヘルスチェック用エンドポイント
+@app.route('/health')
+def health():
+    return "ok", 200  # 200 OKを返すことで正常と判定される
 
 def run():
     app.run(host='0.0.0.0', port=8080)
-
 
 def keep_alive():
     t = Thread(target=run)
